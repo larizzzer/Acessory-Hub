e91e636f559703055c32c80fa129c9a494eb99b2
Larissa Gomes <larissagomesgaspar@gmail.com>
2025-12-15 17:49:36 -0300

    Criação completa de todas as tabelas necessárias

--- Tabelas.sql
+++ Tabelas.sql
@@ -14,3 +14,66 @@ CREATE TABLE Lojas(
     Data_Abertura DATE NOT NULL,
     Status VARCHAR(10) NOT NULL
 );
+
+CREATE TABLE Funcionarios(
+	Id INT AUTO_INCREMENT PRIMARY KEY,
+    Id_Loja INT,
+    Nome VARCHAR(20) NOT NULL,
+    Sobrenome VARCHAR(20) NOT NULL,
+    Cargo VARCHAR(50) NOT NULL,
+    Email VARCHAR(25) NOT NULL UNIQUE,
+    Percentual_Comissao DECIMAL (5,2) NOT NULL,
+    Data_Inicio DATE NOT NULL,
+    Status VARCHAR(20) NOT NULL DEFAULT 'Ativo',
+    Data_Demissao DATE DEFAULT NULL
+);
+
+CREATE TABLE Produtos(
+	Id INT AUTO_INCREMENT PRIMARY KEY,
+    Id_Lojas INT,
+    Nome_Produto VARCHAR(100) NOT NULL,
+    Tipo_Acessorio VARCHAR(50) NOT NULL,
+    Preco DECIMAL(5,2) NOT NULL,
+    Descricao TEXT,
+    Qtde_Estoque INT NOT NULL,
+    Data_Cadastro DATE NOT NULL,
+    Marca VARCHAR(25),
+    Categoria VARCHAR(50) NOT NULL,
+    Ativo VARCHAR(20) NOT NULL DEFAULT 'Ativo'
+);
+
+CREATE TABLE Pedidos(
+	Id INT AUTO_INCREMENT PRIMARY KEY,
+    Id_Funcionarios INT,
+    Data_Pedido DATE NOT NULL,
+    Valor_Total DECIMAL(8,2) NOT NULL,
+    Status_Pedido VARCHAR(20) NOT NULL DEFAULT 'Não Realizado',
+    Forma_Pagamento VARCHAR(30) NOT NULL DEFAULT 'Dinheiro',
+    Tipo_Venda VARCHAR(20) NOT NULL DEFAULT 'Presencial',
+    Observacao TEXT,
+    Desconto DECIMAL(5,2),
+    FOREIGN KEY (Id_Funcionarios) REFERENCES Funcionarios(Id)
+);
+
+CREATE TABLE Itens_do_Pedido(
+	Id_Produto INT,
+    Id_Pedido INT,
+    Quantidade INT NOT NULL,
+    Desconto_Item DECIMAL(5,2),
+    Obeservacao_Item TEXT,
+    Subtotal DECIMAL(8,2) NOT NULL,
+    FOREIGN KEY (Id_Produto) REFERENCES Produtos(Id),
+    FOREIGN KEY (Id_Pedido) REFERENCES Pedidos(Id)
+);
+
+DROP TABLE Funcionarios;
+DROP TABLE Produtos;
+
+ALTER TABLE Funcionarios ADD CONSTRAINT Id_Loja FOREIGN KEY (Id_Loja) REFERENCES Lojas(Id);
+ALTER TABLE Produtos ADD CONSTRAINT Id_Lojas FOREIGN KEY (Id_Lojas) REFERENCES Lojas(Id);
+
+SELECT * FROM Lojas;
+SELECT * FROM Funcionarios;
+SELECT * FROM Produtos;
+SELECT * FROM Pedidos;
+SELECT * FROM Itens_do_Pedido;
