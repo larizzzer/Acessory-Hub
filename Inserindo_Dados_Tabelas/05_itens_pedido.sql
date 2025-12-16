8bd5d3fb9a838dc80ecf28c615ca3c500e5768fc
Larissa Gomes <larissagomesgaspar@gmail.com>
2025-12-16 14:42:05 -0300

    Inseri dados na tabela de itens do pedido

---
+++ Inserindo_Dados_Tabelas/05_itens_pedido.sql
@@ -0,0 +1,14 @@
+INSERT INTO Itens_do_Pedido (Id_Produto, Id_Pedido, Quantidade, Desconto_Item, Obeservacao_Item, Subtotal)
+VALUES (1, 20, 2, NULL, NULL, 79.80),
+	   (4, 20, 1, 5.00, 'Desconto aplicado manualmente', 94.90),
+	   (3, 21, 1, NULL, NULL, 59.90),
+	   (6, 21, 2, NULL, 'Cliente pediu embalagem extra', 139.80),
+	   (2, 22, 3, 10.00, NULL, 89.70),
+	   (5, 23, 1, NULL, NULL, 99.90),
+	   (8, 23, 2, NULL, NULL, 120.00),
+	   (7, 24, 1, NULL, 'Produto fora de linha', 39.99),
+	   (9, 25, 2, NULL, NULL, 69.90),
+	   (10, 26, 1, NULL, 'Erro de registro', 79.90),
+	   (11, 27, 1, NULL, NULL, 120.00),
+	   (12, 28, 1, NULL, NULL, 24.90),
+	   (13, 29, 1, 5.00, 'Venda promocional', 42.00);
