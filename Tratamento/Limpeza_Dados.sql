690c5deb750832306e21b6cdf928e6500da97467
Larissa Gomes <larissagomesgaspar@gmail.com>
2025-12-17 12:55:58 -0300

    Criação de visualização de padronização de status da tabela funcionários

--- Tratamento/Limpeza_Dados.sql
+++ Tratamento/Limpeza_Dados.sql
@@ -1,4 +1,17 @@
 USE AcessoryHub;
 
 SELECT Nome, Sobrenome, CONCAT(UCASE(SUBSTRING(Status, 1, 1)), LOWER(SUBSTRING(Status, 2))) AS Status
-FROM Funcionarios; -- Coloca a primeira letra maiúscula
+FROM Funcionarios; -- Verificando se funciona para a primeira letra ficar maiúscula e o restante minúsculo
+
+CREATE VIEW VW_Funcionarios_Padronizados AS
+SELECT Id,
+	   Id_Loja AS Loja,
+	   Nome, 
+	   Sobrenome,
+       CONCAT(UCASE(SUBSTRING(Status, 1, 1)), LOWER(SUBSTRING(Status, 2))) AS Status -- Coloca a primeira letra maiúscula
+FROM Funcionarios;
+
+SELECT * FROM VW_Funcionarios_Padronizados
+ORDER BY Nome; -- Verifica se a view foi criada corretamente e ordena por ordem alfabética
+
+DROP VIEW VW_Funcionarios_Padronizados; -- Apaga a view caso queira adcionar mais alguma coluna
