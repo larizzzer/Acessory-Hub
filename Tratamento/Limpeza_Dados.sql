d274ec509b4bf641d17f7062891d45ba0ecf5024
Larissa Gomes <larissagomesgaspar@gmail.com>
2025-12-17 13:03:33 -0300

    Criação de visualização de padronização da tabela lojas

--- Tratamento/Limpeza_Dados.sql
+++ Tratamento/Limpeza_Dados.sql
@@ -1,5 +1,6 @@
 USE AcessoryHub;
 
+-- TABELA FUNCIONÁRIOS
 SELECT Nome, Sobrenome, CONCAT(UCASE(SUBSTRING(Status, 1, 1)), LOWER(SUBSTRING(Status, 2))) AS Status
 FROM Funcionarios; -- Verificando se funciona para a primeira letra ficar maiúscula e o restante minúsculo
 
@@ -15,3 +16,16 @@ SELECT * FROM VW_Funcionarios_Padronizados
 ORDER BY Nome; -- Verifica se a view foi criada corretamente e ordena por ordem alfabética
 
 DROP VIEW VW_Funcionarios_Padronizados; -- Apaga a view caso queira adcionar mais alguma coluna
+
+-- TABELA LOJAS
+SELECT * FROM Lojas;
+SELECT * FROM VW_Lojas_Padronizadas;
+
+CREATE VIEW VW_Lojas_Padronizadas AS
+SELECT Id,
+	   Nome_Loja AS Loja,
+       Nome_Dono AS 'Proprietário',
+       Cidade,
+       Cnpj,
+       CONCAT(UCASE(SUBSTRING(Status, 1, 1)), LOWER(SUBSTRING(Status, 2))) AS Status
+FROM Lojas;
