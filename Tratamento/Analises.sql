1d314009752306bbd2ebb061fc36f3a48c164efd
Larissa Gomes <larissagomesgaspar@gmail.com>
2025-12-20 11:03:47 -0300

    Começo das análise na parte de vendas e de desempenho

--- Tratamento/Analises.sql
+++ Tratamento/Analises.sql
@@ -1,6 +1,5 @@
 USE AcessoryHub;
 
--- Qual foi o faturamento total por loja em um determinado período?
 SELECT * FROM VW_Lojas_Padronizadas;
 SELECT * FROM Lojas;
 SELECT * FROM Pedidos;
@@ -8,10 +7,14 @@ SELECT * FROM VW_Padronizacao_Pedidos;
 SELECT * FROM VW_Funcionarios_Padronizados;
 SELECT * FROM Funcionarios;
 
-SELECT l.Loja AS Loja,
-	   f.Nome AS Nome,
-       CONCAT(UCASE(SUBSTRING(Status_Pedido, 1, 1)), LOWER(SUBSTRING(Status_Pedido, 2))) AS Status,
-       AVG(p.Valor_Total) AS Venda
-FROM VW_Lojas_Padronizadas l JOIN Funcionarios f 
-ON l.Id = f.Id_Loja JOIN Pedidos p ON f.Id = p.Id
-GROUP BY l.Loja;
+-- VENDAS E DESEMPENHO
+-- Qual foi o faturamento total por loja em um determinado período?
+SELECT 
+    l.Loja AS Loja,
+    f.Nome AS Nome,
+    CONCAT(UCASE(SUBSTRING(p.Status_Pedido, 1, 1)), LOWER(SUBSTRING(p.Status_Pedido, 2))) AS Status,
+    ROUND(AVG(p.Valor_Total), 2) AS "Média de Venda"
+FROM VW_Lojas_Padronizadas l JOIN Funcionarios f ON l.Id = f.Id_Loja
+JOIN Pedidos p ON f.Id = p.Id_Funcionarios
+WHERE p.Status_Pedido = 'Realizado'
+GROUP BY l.Loja, f.Nome, p.Status_Pedido;
