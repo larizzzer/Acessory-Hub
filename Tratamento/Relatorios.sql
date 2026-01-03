b816d0e16508e9cb346ff0b4b1aa8ebd5807b142
Larissa Gomes <larissagomesgaspar@gmail.com>
2026-01-02 21:48:50 -0300

    Feat: Inserindo relatórios de mais 2 lojas

--- Tratamento/Relatorios.sql
+++ Tratamento/Relatorios.sql
@@ -1,10 +1,9 @@
 USE AcessoryHub;
 
--- Relatórios Gerenciais por cada loja
+-- RELATÓRIOS GERENCIAIS POR CADA LOJA
 
 -- Relatórios da Shine Acessórios
 -- Sempre considerando os pedidos realizados
-
 SELECT 
       l.Nome_Loja AS Loja,
       COUNT(p.Id) AS 'Total de pedidos',
@@ -40,7 +39,85 @@ WHERE p.Status_Pedido = 'Realizado' AND l.Nome_Loja = 'Shine Acessórios'
 GROUP BY l.Nome_Loja, pr.Nome_Produto
 ORDER BY Faturamento_Total DESC;
 
+-- Relatórios da Urban Style Bijus
+-- Sempre considerando os pedidos realizados
+
+SELECT 
+      l.Nome_Loja AS Loja,
+      COUNT(p.Id) AS 'Total de pedidos',
+      ROUND(SUM(p.Valor_Total), 2) AS 'Faturamento Total',
+      ROUND(AVG(p.Valor_Total), 2) AS 'Ticket médio'
+FROM Lojas l INNER JOIN Funcionarios f ON l.Id = f.Id_Loja
+INNER JOIN Pedidos p ON f.Id = p.Id_Funcionarios
+WHERE p.Status_Pedido = 'Realizado' AND l.Nome_Loja = 'Urban Style Bijus'
+GROUP BY l.Nome_Loja;
+
+-- Total de vendas por funcionário
+SELECT 
+      l.Nome_Loja AS Loja,
+      f.Nome AS 'Funcionário',
+      COUNT(p.Id) AS 'Total de pedidos',
+      ROUND(SUM(p.Valor_Total), 2) AS 'Total vendido',
+      ROUND(AVG(p.Valor_Total), 2) AS 'Ticket médio'
+FROM Funcionarios f INNER JOIN Lojas l ON f.Id_Loja = l.Id
+INNER JOIN Pedidos p ON f.Id = p.Id_Funcionarios
+WHERE p.Status_Pedido = 'Realizado' AND l.Nome_Loja = 'Urban Style Bijus'
+GROUP BY l.Nome_Loja, f.Nome;
+
+-- Produtos com maior impacto nas vendas
+SELECT 
+    l.Nome_Loja AS Loja,
+    pr.Nome_Produto AS Produto,
+    SUM(ip.Quantidade) AS 'Quantidade vendida',
+    ROUND(SUM(ip.Subtotal), 2) AS Faturamento_Total
+FROM Itens_do_Pedido ip INNER JOIN Produtos pr ON ip.Id_Produto = pr.Id
+INNER JOIN Pedidos p ON ip.Id_Pedido = p.Id INNER JOIN Funcionarios f ON p.Id_Funcionarios = f.Id
+INNER JOIN Lojas l ON f.Id_Loja = l.Id
+WHERE p.Status_Pedido = 'Realizado' AND l.Nome_Loja = 'Urban Style Bijus'
+GROUP BY l.Nome_Loja, pr.Nome_Produto
+ORDER BY Faturamento_Total DESC;
+
+-- Relatórios da Essence Biju
+-- Sempre considerando os pedidos realizados
+
+SELECT 
+      l.Nome_Loja AS Loja,
+      COUNT(p.Id) AS 'Total de pedidos',
+      ROUND(SUM(p.Valor_Total), 2) AS 'Faturamento Total',
+      ROUND(AVG(p.Valor_Total), 2) AS 'Ticket médio'
+FROM Lojas l INNER JOIN Funcionarios f ON l.Id = f.Id_Loja
+INNER JOIN Pedidos p ON f.Id = p.Id_Funcionarios
+WHERE p.Status_Pedido = 'Realizado' AND l.Nome_Loja = 'Essence Biju'
+GROUP BY l.Nome_Loja;
+
+-- Total de vendas por funcionário
+SELECT 
+      l.Nome_Loja AS Loja,
+      f.Nome AS 'Funcionário',
+      COUNT(p.Id) AS 'Total de pedidos',
+      ROUND(SUM(p.Valor_Total), 2) AS 'Total vendido',
+      ROUND(AVG(p.Valor_Total), 2) AS 'Ticket médio'
+FROM Funcionarios f INNER JOIN Lojas l ON f.Id_Loja = l.Id
+INNER JOIN Pedidos p ON f.Id = p.Id_Funcionarios
+WHERE p.Status_Pedido = 'Realizado' AND l.Nome_Loja = 'Essence Biju'
+GROUP BY l.Nome_Loja, f.Nome;
+
+-- Produtos com maior impacto nas vendas
+SELECT 
+    l.Nome_Loja AS Loja,
+    pr.Nome_Produto AS Produto,
+    SUM(ip.Quantidade) AS 'Quantidade vendida',
+    ROUND(SUM(ip.Subtotal), 2) AS Faturamento_Total
+FROM Itens_do_Pedido ip INNER JOIN Produtos pr ON ip.Id_Produto = pr.Id
+INNER JOIN Pedidos p ON ip.Id_Pedido = p.Id INNER JOIN Funcionarios f ON p.Id_Funcionarios = f.Id
+INNER JOIN Lojas l ON f.Id_Loja = l.Id
+WHERE p.Status_Pedido = 'Realizado' AND l.Nome_Loja = 'Essence Biju'
+GROUP BY l.Nome_Loja, pr.Nome_Produto
+ORDER BY Faturamento_Total DESC;
+
+
 -- RELATÓRIOS GLOBAIS (VISÃO CONSOLIDADA DE TODAS AS LOJAS)
+
 -- Impacto dos descontos
 SELECT 
       ROUND(SUM(IFNULL(p.Desconto, 0)), 2) AS Total_Descontos,
